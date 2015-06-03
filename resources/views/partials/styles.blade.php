1ee552b744e22c5bdbb4ec8f9a2ecf51d7283f6e
frkodes <frkalderon@gmail.com>
2015-06-03 15:55:30 -0500

    Compressing for production

--- resources/views/partials/styles.blade.php
+++ resources/views/partials/styles.blade.php
@@ -1,2 +1,2 @@
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="{{ elixir('css/all.min.css')}}">
