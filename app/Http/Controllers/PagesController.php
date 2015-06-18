589ced2c22c0a6a3a65734a4eab7a7d0bcc36d13
frkodes <frkalderon@gmail.com>
2015-06-18 15:05:25 -0500

    PagesController fix

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,14 +12,9 @@ class PagesController extends Controller {
 	public function sendmail2()
 	{		
 
-		// return 'ok from pages controller';
-		// return 'Mandrill key: '.getenv('MANDRILL_KEY').'\nmail driver: '.getenv('MAIL_DRIVER');
-
-
 		Mail::send('emails.contacto', [], function($message){
 			$message->to('frkalderon@gmail.com')->subject('Atelier contacto');
 		});
-
 	}
 
 }
