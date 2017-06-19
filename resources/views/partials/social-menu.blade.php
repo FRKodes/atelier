24b802161613594962b1eda2d3793a267e53d311
frkodes <frkalderon@gmail.com>
2017-06-19 18:39:30 -0500

    Adding the credits

--- resources/views/partials/social-menu.blade.php
+++ resources/views/partials/social-menu.blade.php
@@ -3,3 +3,5 @@
 	<li><a class="icon-06" target="_blank" href="https://twitter.com/AtelierLab"></a></li>
 	<li><a class="icon-05" target="_blank" href="https://instagram.com/Atelier_Lab/"></a></li>
 </ul>
+
+<p class="bt_credits">Desarrollado por <a target="_blank" class="white" href="http://www.blueterrier.mx"><strong>Blue Terrier</strong></a></p>
